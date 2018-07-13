745b38c382e1b193a6827d4b90a3e643d8f18c59
rodrigobrossi <brossi.consulting@gmail.com>
2018-07-13 11:10:55 -0300

    Initial version

--- app/router.js
+++ app/router.js
@@ -7,6 +7,8 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('products');
+  this.route('clients');
 });
 
 export default Router;
---
+++ app/routes/products.js
@@ -0,0 +1,8 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+
+    model(){
+        return ['Caixa de som','Mesa 1', 'Mesa 2'];
+    }
+});
