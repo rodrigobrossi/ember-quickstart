fee89c95dc725216570a0e91fc919694e49b9848
rodrigobrossi <brossi.consulting@gmail.com>
2018-07-13 11:39:26 -0300

    Initial version

--- app/components/client-list.js
+++ app/components/client-list.js
@@ -1,4 +1,19 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+
+    actions: {
+        showClient(client) {
+            alert("Name:" + cient);
+        },
+        deleteClient(client) {
+            var txt;
+            var r = confirm("Do you want to delete: " + client+" registry");
+            if (r == true) {
+                txt = "You pressed OK!";
+            } else {
+                txt = "You pressed Cancel!";
+            }
+        }
+    }
 });
