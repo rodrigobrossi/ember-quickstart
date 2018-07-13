b2e264c70b99783b48641351ee0555d428ce5674
rodrigobrossi <brossi.consulting@gmail.com>
2018-07-13 11:19:52 -0300

    Initial version

--- app/routes/clients.js
+++ app/routes/clients.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    model(){
+        return ['Rodrigo Luis Nolli Brossi', 'Rafael Santos', 'Daniel Vilas Boas'];
+    }
 });
--- app/routes/products.js
+++ app/routes/products.js
@@ -1,7 +1,6 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-
     model(){
         return ['Caixa de som','Mesa 1', 'Mesa 2'];
     }
